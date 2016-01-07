91c1ed10937ddbf3590d80faf94e177c84b99d83
Steve Leathers <sayhello@steveleathers.com>
2016-01-07 14:53:42 -0800

    fixed functionality for 3 images

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
   $('#person1 .green-button').click(function(){
-    $(this).parent().parent().children('p.correct').removeClass('hidden');
-    $(this).parent().parent().children('p.incorrect').addClass('sr-only');
+    $(this).parent().parent().children('div.overlay').children('p.correct').removeClass('hidden');
+    $(this).parent().parent().children('div.overlay').children('p.incorrect').addClass('sr-only');
   });
   $('#person1 .red-button').click(function(){
-    $(this).parent().parent().children('p.incorrect').removeClass('hidden');
-    $(this).parent().parent().children('p.correct').addClass('sr-only');
+    $(this).parent().parent().children('div.overlay').children('p.incorrect').removeClass('hidden');
+    $(this).parent().parent().children('div.overlay').children('p.correct').addClass('sr-only');
+  });
+
+  $('#person2 .green-button').click(function(){
+    $(this).parent().parent().children('div.overlay').children('p.correct').removeClass('hidden');
+    $(this).parent().parent().children('div.overlay').children('p.incorrect').addClass('sr-only');
+  });
+  $('#person2 .red-button').click(function(){
+    $(this).parent().parent().children('div.overlay').children('p.incorrect').removeClass('hidden');
+    $(this).parent().parent().children('div.overlay').children('p.correct').addClass('sr-only');
+  });
+
+  $('#person3 .green-button').click(function(){
+    $(this).parent().parent().children('div.overlay').children('p.correct').removeClass('hidden');
+    $(this).parent().parent().children('div.overlay').children('p.incorrect').addClass('sr-only');
+  });
+  $('#person3 .red-button').click(function(){
+    $(this).parent().parent().children('div.overlay').children('p.incorrect').removeClass('hidden');
+    $(this).parent().parent().children('div.overlay').children('p.correct').addClass('sr-only');
   });
 });
