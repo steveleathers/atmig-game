25d55f7ca22f0757ee732bbf31d98aaadb81b34c
Steve Leathers <sayhello@steveleathers.com>
2016-01-07 14:24:16 -0800

    changed functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $("#person1 .green-button").click(function(){
     $(this).parent().parent().children('p.correct').removeClass('hidden');
+    $(this).parent().parent().children('p.incorrect').addClass('sr-only');
   });
   $('#person1 .red-button').click(function(){
     $(this).parent().parent().children('p.incorrect').removeClass('hidden');
+    $(this).parent().parent().children('p.correct').addClass('sr-only');
   });
 });
