8a711dfab3a2a8c623600bfcc313c1d1e70f05f9
Steve Leathers <steveleathers@gmail.com>
2016-02-08 17:01:35 -0800

    removed a few dom traversals

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
   $('#person1').click(function(){
-    $(this).parent().parent().children('div.overlay').children('p.correct').removeClass('hidden');
+    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
   });
 
   $('#person2').click(function(){
     $(this).parent().parent().children('div.overlay').children('p.correct').removeClass('hidden');
     $(this).parent().parent().children('div.overlay').children('p.incorrect').addClass('sr-only');
-  });
-  $('#person2').click(function(){
-    $(this).parent().parent().children('div.overlay').children('p.incorrect').removeClass('hidden');
-    $(this).parent().parent().children('div.overlay').children('p.correct').addClass('sr-only');
   });
 
   $('#person3 .green-button').click(function(){
