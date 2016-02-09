bc9fcfe86e6814294bd86646fcb9f607a1b0fa5c
Steve Leathers <steveleathers@gmail.com>
2016-02-08 17:14:53 -0800

    added content

--- js/script.js
+++ js/script.js
@@ -10,4 +10,26 @@ $(document).ready(function() {
   $('#person3').click(function(){
     $(this).children('div.overlay').children('p.correct').removeClass('hidden');
   });
+  $('#person4').click(function(){
+    $(this).children('div.overlay').children('p.incorrect').removeClass('hidden');
+  });
+
+  $('#person5').click(function(){
+    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
+  });
+
+  $('#person6').click(function(){
+    $(this).children('div.overlay').children('p.incorrect').removeClass('hidden');
+  });
+  $('#person7').click(function(){
+    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
+  });
+
+  $('#person8').click(function(){
+    $(this).children('div.overlay').children('p.incorrect').removeClass('hidden');
+  });
+
+  $('#person9').click(function(){
+    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
+  });
 });
