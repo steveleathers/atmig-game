694efeabf35b4cbe35b83e0a160599b96556025f
Steve Leathers <steveleathers@gmail.com>
2016-03-04 16:04:35 -0800

    added new employees, changed click functionality

--- js/script.js
+++ js/script.js
@@ -1,35 +1,5 @@
 $(document).ready(function() {
-  $('#person1').click(function(){
-    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
-  });
-
-  $('#person2').click(function(){
-    $(this).children('div.overlay').children('p.incorrect').removeClass('hidden');
-  });
-
-  $('#person3').click(function(){
-    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
-  });
-  $('#person4').click(function(){
-    $(this).children('div.overlay').children('p.incorrect').removeClass('hidden');
-  });
-
-  $('#person5').click(function(){
-    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
-  });
-
-  $('#person6').click(function(){
-    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
-  });
-  $('#person7').click(function(){
-    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
-  });
-
-  $('#person8').click(function(){
-    $(this).children('div.overlay').children('p.correct').removeClass('hidden');
-  });
-
-  $('#person9').click(function(){
-    $(this).children('div.overlay').children('p.incorrect').removeClass('hidden');
+  $('.person').click(function(){
+      $(this).children('div.overlay').children('p').toggleClass('hidden');
   });
 });
